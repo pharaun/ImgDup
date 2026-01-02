9697ec3a4439c543d483b0b732d073554bcd4259
Anja Berens <pharaun666@gmail.com>
2026-01-02 12:34:00 -0600

    Initial Commit

---
+++ ImgDup/ImgDupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImgDupApp.swift
+//  ImgDup
+//
+//  Created by Anja Berens on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ImgDupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
